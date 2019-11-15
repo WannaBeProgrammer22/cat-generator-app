7287e6fadcc39015634ba428b7416e28b2986df3
Jerwin <jerwinfernandez04@gmail.com>
2019-11-16 02:59:31 +0800

    Creating a simple cat generator image

---
+++ src/cat.js
@@ -0,0 +1,32 @@
+class Cat {
+
+  constructor(container, api_key) {
+    this.container = container;
+    this.resourceURI = 'https://api.thecatapi.com/v1/images/search';
+    this.api_key = api_key;
+    this.img = document.createElement('img');
+    this.setPhoto();
+  }
+
+  setPhoto() {
+    this.getPhoto()
+      .then(cat => {
+        this.img.alt = 'Active Cat';
+        this.img.className = 'img-fluid'
+        this.img.src = cat.url;
+        this.container.appendChild(this.img);
+      });
+  }
+
+  async getPhoto() {
+
+    const response = await fetch(`${this.resourceURI}?api_key=${this.api_key}`);
+    const data = await response.json();
+
+    return data[0];
+  }
+
+
+}
+
+export { Cat as default };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('test')
+import './styles/style.css';
+import "@babel/polyfill";
+import Cat from './cat';
+
+const photoContainer = document.querySelector('.photo-container');
+const api_key = 'fd4bb618-9046-4cc9-81be-85dd54033925';
+const nextButton = document.querySelector('.next');
+const automaticBtutton = document.querySelector('#automatic');
+const stopButton = document.querySelector('#stop');
+let automatic;
+
+const cat = new Cat(photoContainer, api_key);
+
+nextButton.addEventListener('click', () => {
+  cat.setPhoto();
+})
+
+automaticBtutton.addEventListener('click', () => {
+  automaticBtutton.textContent = 'Stop';
+  automaticBtutton.classList.remove('btn-primary');
+  automaticBtutton.classList.add('btn-danger');
+
+  cat.setPhoto();
+
+  if (automaticBtutton.id === 'automatic') {
+    automatic = setInterval(() => {
+      cat.setPhoto();
+      automaticBtutton.id = 'stop';
+    }, 5000);;
+  }
+});
+
+document.querySelector('.react-button').addEventListener('click', e => {
+  let button = e.target;
+  if (button.id === 'stop') {
+    clearInterval(automatic);
+
+    automaticBtutton.textContent = 'Automatic Next';
+    automaticBtutton.classList.remove('btn-danger');
+    automaticBtutton.classList.add('btn-primary');
+    automaticBtutton.id = 'automatic';
+
+  }
+})
