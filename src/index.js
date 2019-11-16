75ce34118ad04a48400620cbd0645ce1fca1e383
Jerwin <jerwinfernandez04@gmail.com>
2019-11-16 23:02:19 +0800

    Refactoring index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Cat from './cat';
 const photoContainer = document.querySelector('.photo-container');
 const api_key = 'fd4bb618-9046-4cc9-81be-85dd54033925';
 const nextButton = document.querySelector('.next');
-const automaticBtutton = document.querySelector('#automatic');
+const automaticButton = document.querySelector('#automatic');
 const stopButton = document.querySelector('#stop');
 let automatic;
 
@@ -15,30 +15,49 @@ nextButton.addEventListener('click', () => {
   cat.setPhoto();
 })
 
-automaticBtutton.addEventListener('click', () => {
-  automaticBtutton.textContent = 'Stop';
-  automaticBtutton.classList.remove('btn-primary');
-  automaticBtutton.classList.add('btn-danger');
-
+automaticButton.addEventListener('click', () => {
   cat.setPhoto();
 
-  if (automaticBtutton.id === 'automatic') {
+  if (automaticButton.id === 'automatic') {
+    // transform the button into stop button
+    startAutomatic(automaticButton);
+
+    // set automatic next
     automatic = setInterval(() => {
       cat.setPhoto();
-      automaticBtutton.id = 'stop';
     }, 6000);
   }
 });
 
-document.querySelector('.react-button').addEventListener('click', e => {
-  let button = e.target;
-  if (button.id === 'stop') {
-    clearInterval(automatic);
+stopButton.addEventListener('click', () => {
+  // clear automatic next
+  clearInterval(automatic);
 
-    automaticBtutton.textContent = 'Automatic Next';
-    automaticBtutton.classList.remove('btn-danger');
-    automaticBtutton.classList.add('btn-primary');
-    automaticBtutton.id = 'automatic';
+  // transform stop button to automatic button
+  stopAutomatic(stopButton);
+});
 
-  }
-})
+const startAutomatic = (btn) => {
+  btn.textContent = 'Stop';
+  btn.classList.remove('btn-primary');
+  btn.classList.add('btn-danger');
+  btn.id = 'stop';
+
+  // show/hide button
+  toggleButtons()
+};
+
+const stopAutomatic = (btn) => {
+  automaticButton.textContent = 'Automatic Next';
+  automaticButton.classList.remove('btn-danger');
+  automaticButton.classList.add('btn-primary');
+  automaticButton.id = 'automatic';
+
+  // show/hide button
+  toggleButtons()
+}
+
+const toggleButtons = () => {
+  automaticButton.classList.toggle('d-none');
+  stopButton.classList.toggle('d-none');
+};
